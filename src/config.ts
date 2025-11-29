b9b9c7e203f7967e3b133c0d82d27fbfea44d215
nakorchy <455965893@qq.com>
2025-11-29 17:07:30 +0800

    更新

--- src/config.ts
+++ src/config.ts
@@ -20,10 +20,10 @@ import { LinkPreset } from "./types/config";
 const SITE_LANG = "en"; // 语言代码，例如：'en', 'zh_CN', 'ja' 等。
 const SITE_TIMEZONE = 8; //设置你的网站时区 from -12 to 12 default in UTC+8
 export const siteConfig: SiteConfig = {
-	title: "Mizuki",
+	title: "Nakorchy",
 	subtitle: "One demo website",
-	siteURL: "https://mizuki.mysqil.com/", // 请替换为你的站点URL，以斜杠结尾
-	siteStartDate: "2025-01-01", // 站点开始运行日期，用于站点统计组件计算运行天数
+	siteURL: "https://nakorchy.vercel.app/", // 请替换为你的站点URL，以斜杠结尾
+	siteStartDate: "2025-11-29", // 站点开始运行日期，用于站点统计组件计算运行天数
 
 	timeZone: SITE_TIMEZONE,
 
